6f6a19d4f590d9bc85855eabeb134e0a00acab22
Gustavo Luiz <70780066+luiz-lgrp@users.noreply.github.com>
2022-06-27 12:21:18 -0300

    first commit

---
+++ src/Course.Customers.API/Controllers/CustomersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Course.Customers.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetById()
+        {
+            return Ok();
+        }
+    }
+}
